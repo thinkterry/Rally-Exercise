ad52546329ada7fb80d9426211989cfb7bec1ce8
Terry <think@terry>
2012-10-03 08:49:31 -0600

    even more ugly but the teens are working

--- exercise.js
+++ exercise.js
@@ -6,7 +6,7 @@ function convert(amount) {
 		tens = 'Twenty';
 	}
 	
-	if (amount <= 10) {
+	if (amount < 20) {
 		ones = getOnes(amount);
 		
 		// Capitalize first letter
@@ -42,6 +42,24 @@ function getOnes(ones) {
 			return 'nine';
 		case 10:
 			return 'ten';
+		case 11:
+			return 'eleven';
+		case 12:
+			return 'twelve';
+		case 13:
+			return 'thirteen';
+		case 14:
+			return 'fourteen';
+		case 15:
+			return 'fifteen';
+		case 16:
+			return 'sixteen';
+		case 17:
+			return 'seventeen';
+		case 18:
+			return 'eighteen';
+		case 19:
+			return 'nineteen';
 		default:
 			break;
 	}
--- tests/tests.js
+++ tests/tests.js
@@ -14,6 +14,7 @@ test('simple tests', function () {
 	strictEqual(convert(2), 'Two');
 	strictEqual(convert(9), 'Nine');
 	strictEqual(convert(10), 'Ten');
+	strictEqual(convert(11), 'Eleven');
 	strictEqual(convert(20), 'Twenty');
 	strictEqual(convert(21), 'Twenty-one');
 	strictEqual(convert(22), 'Twenty-two');
