002bd504b35ec942a997f88aea83125511f5baef
Terry <think@terry>
2012-10-03 08:15:26 -0600

    wrote first test and code

--- tests/tests.js
+++ tests/tests.js
@@ -1,3 +1,14 @@
+/*
+// copy/paste
+test(' test', function () {
+	ok(, '');
+});
+*/
+
 test('hello test', function () {
 	ok(true === true, 'true is true');
+});
+
+test('simple tests', function () {
+	strictEqual(convert(1), 'One');
 });
